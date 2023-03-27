d008bb684d3f69c743fcb9e9f62e4eb5c0000eee
Alexaloka <103824973+Alexaloka@users.noreply.github.com>
2023-03-27 13:11:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+noseX=0;
+noseY=0;
 function prelosd(){
+
 
 }
 function setup(){
@@ -12,6 +15,9 @@ function setup(){
 }
 function draw(){
 image(video, 0, 0, 300, 300);
+fill(255,0,0);
+stroke(255,0,0);
+circle(noseX,noseY,20);
 }
 function take_snapshot()
 {
@@ -26,6 +32,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+
         console.log('nose x = ' + results[0].pose.nose.x);
         console.log('nose y = ' + results[0].pose.nose.y);
 
